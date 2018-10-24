aa4bf135a7110c74540dd4a19b2d0cc68a9dfb8a
dnsnat <demonelf@madhex.com>
2018-10-24 11:08:50 +0800

    1.初始化

---
+++ static/pure-css3-colorful-loading/js/index.js
@@ -0,0 +1,5 @@
+(function($){
+  $('.trigger').click(function(){
+    $('#ajaxloader1, .outer, .inner, .barlittle, .pointcircle, .bar, #loadpulse div, #shadowloader span, .loadingtext span').toggleClass('stop');
+  });
+})(jQuery);
