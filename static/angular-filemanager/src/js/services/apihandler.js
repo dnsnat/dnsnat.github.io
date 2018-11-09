aeff76be3a5fca49eb263e7c26eedef4a8e881e4
dnsnat <demonelf@madhex.com>
2018-11-09 11:21:21 +0800

    1.angular适配认证

--- static/angular-filemanager/src/js/services/apihandler.js
+++ static/angular-filemanager/src/js/services/apihandler.js
@@ -1,37 +1,10 @@
-function getCookie(c_name)
-{
-    if (document.cookie.length>0)
-      {
-      c_start=document.cookie.indexOf(c_name + "=")
-      if (c_start!=-1)
-        {
-        c_start=c_start + c_name.length+1
-        c_end=document.cookie.indexOf(";",c_start)
-        if (c_end==-1) c_end=document.cookie.length
-        return unescape(document.cookie.substring(c_start,c_end))
-        }
-      }
-    return ""
-}
-
-$(document).ajaxSend(function (event, xhr, settings) {
-    settings.xhrFields = {
-        withCredentials: true
-    };
-});
-
 (function(angular) {
     'use strict';
     angular.module('FileManagerApp').service('apiHandler', ['$http', '$q', '$window', '$translate', '$httpParamSerializer', 'Upload',
         function ($http, $q, $window, $translate, $httpParamSerializer, Upload) {
 
         $http.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-        //$http.defaults.withCredentials = true;
-        $http.defaults.withCredentials = true;
-        $http.defaults.headers.common['Authorization'] = "89757";
-        //$http.defaults.headers.common['Cookie'] = 'sessionid=' + getCookie('sessionid');
-        //$httpProvider.defaults.headers.common['Authorization'] = "89757";
-        //$httpProvider.defaults.headers.post = {'my-header':'value'};
+
         var ApiHandler = function() {
             this.inprocess = false;
             this.asyncSuccess = false;
