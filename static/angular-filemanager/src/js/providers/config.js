482c60e538fec30b06db3f10151717341147f541
dnsnat <demonelf@madhex.com>
2018-11-09 09:38:48 +0800

    1.angular适配认证

--- static/angular-filemanager/src/js/providers/config.js
+++ static/angular-filemanager/src/js/providers/config.js
@@ -1,21 +1,22 @@
-        function getCookie(c_name)
+function getCookie(c_name)
+{
+    if (document.cookie.length>0)
+      {
+      c_start=document.cookie.indexOf(c_name + "=")
+      if (c_start!=-1)
         {
-            if (document.cookie.length>0)
-              {
-              c_start=document.cookie.indexOf(c_name + "=")
-              if (c_start!=-1)
-                {
-                c_start=c_start + c_name.length+1
-                c_end=document.cookie.indexOf(";",c_start)
-                if (c_end==-1) c_end=document.cookie.length
-                return unescape(document.cookie.substring(c_start,c_end))
-                }
-              }
-            return ""
+        c_start=c_start + c_name.length+1
+        c_end=document.cookie.indexOf(";",c_start)
+        if (c_end==-1) c_end=document.cookie.length
+        return unescape(document.cookie.substring(c_start,c_end))
         }
+      }
+    return ""
+}
 
-        username=getCookie('username');
-        domain=getCookie('serverdomain');
+username=getCookie('username');
+domain=getCookie('serverdomain');
+
 (function(angular) {
     'use strict';
     angular.module('FileManagerApp').provider('fileManagerConfig', function() {
